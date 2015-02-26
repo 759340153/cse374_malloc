62849059a33935e53632600e4cb8f850e08021d5
agingras.washington.edu <agingras@u.washington.edu>
2015-02-25 19:48:59 -0800

    added files

---
+++ freemem.c
@@ -0,0 +1,27 @@
+//freemem.c
+
+//will free the block of memory the passed in pointer points to
+void freemem(void* p) {
+	if (!p) {
+		return;
+	} 
+	//void* prev = findMemorySpot
+	//add p to free
+	//combineSmallBlocks(p, prev);
+	return;
+}
+
+void combineSmallBlocks(void* p, void* prev) {
+	//combine small blocks
+		//if p.next is at the address &p + size of p, combine
+		//if prev + size of prev is the address of p, combine
+	return;
+}
+
+//returns a pointer to the data block that appears before the passed in data block in memory
+//returns null if p should be added front of free
+void* findMemorySpot(void* p) {
+	//if free is empty, pass null
+	//goes until the address of one block + the size of that block is < the address of p and address of the next block is > than the address of p
+	//if the end of free is reached, pass the last block in free
+}
---
+++ mem.h
@@ -0,0 +1,15 @@
+//mem.h
+//declares all the functions that can be used externally
+
+// gets a block of memory of size size
+void* getmem(uinptr_t size); 
+
+//frees the block of memory stored at pointer p
+void freemem(void* p); 
+
+//stores stats about the state of the memory manager in the 3 pointers provided
+//stores total size stored in bytes, total number amount of free storage in bytes, and the number of free blocks 
+void get_mem_stats(uintptr_t* total_size, uintptr_t* total_free, uintptr_t* n_free_blocks); 
+
+//prints a list of all the blocks on the free list into the file provided
+void print_heap(FILE * f);
