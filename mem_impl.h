1a0b3af8c1d8ad244cd1f9e408d44baa6bdf239c
Maximilian Golub <mgolub2@uw.edu>
2015-02-25 22:50:25 -0800

    added memNode struct/typedef and internal stat variables

--- mem_impl.h
+++ mem_impl.h
@@ -5,10 +5,20 @@
 //  Created by Max Golub on 2/25/15.
 //  Copyright (c) 2015 Max Golub. All rights reserved.
 //
-
+#include <intypes.h>
 #ifndef hw6_mem_impl_h
 #define hw6_mem_impl_h
 
+//memNode struct, used to create linked list of free memory
+typedef struct mem {
+    uintptr_t size;
+    uintptr_t next;
+    uintptr_t data;
+} memNode;
 
+//Globals for interally keeping track of stats
+extern int totalFree;
+extern int usedMem;
+extern int freeMem;
 
 #endif
