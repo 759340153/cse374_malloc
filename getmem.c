9054f2706d0c6315aebe42dc14baa4a29006f502
Maximilian Golub <mgolub2@uw.edu>
2015-03-03 23:34:48 -0800

    Fixed alignment issues + other weirdness

--- getmem.c
+++ getmem.c
@@ -10,7 +10,7 @@
 #include "mem_impl.h"
 #include "mem.h"
 
-#define ALIGN 16
+#define ALIGN_VAL 16
 #define SPLIT_SIZE 256 //what size we split a block at
 #define LARGE_BLOCK_SIZE 1024 * 16 //size of initial block
 #define maxOveragePercent .20
@@ -18,7 +18,7 @@
 //funtion protos
 memNode * splitBlock(memNode * block, uintptr_t Splitsize); //split a block into many blocks
 void removeFromFree(memNode * block); //remove a memnode from the freelist
-memNode * mallocData(memNode * block); //give memory to a block
+memNode * mallocData(memNode * block, uintptr_t size); //give memory to a block
 memNode * chooseBlock(memNode * block, memNode * prevBlock, uintptr_t size);
 //globals
 memNode * root;
@@ -30,7 +30,8 @@ int freeMem = 0;
 //grab at least size amount of mem and return a void pointer to the user
 void* getmem(uintptr_t size) {
     if (!root) {
-        mallocData(root);
+        root = mallocData(root, NULL);
+        print_heap(stdout);
         //current = root;
     }
     memNode * choosenBlock = chooseBlock(root, NULL, size);
@@ -39,18 +40,21 @@ void* getmem(uintptr_t size) {
 
 //return
 memNode * splitBlock(memNode * block, uintptr_t splitSize) {
-    int alignment = splitSize % ALIGN;
+    int alignment = splitSize % ALIGN_VAL;
     if(alignment) {
-        splitSize = splitSize + ALIGN - alignment;
+        splitSize = splitSize + ALIGN_VAL - alignment;
+    }
+    else if (splitSize < ALIGN_VAL) {
+        splitSize = ALIGN_VAL;
     }
     uintptr_t oldSize = block->size;
     block->size = splitSize;
-    block->next = (uintptr_t) block+splitSize+2; //maybe want a define for fields?
+    block->next = (uintptr_t) block+splitSize+sizeof(memNode); //maybe want a define for fields?
     memNode newBlock;
-    newBlock.size = oldSize - splitSize; // set the new size.
+    newBlock.size = oldSize-splitSize-sizeof(memNode); // set the new size.
     newBlock.next = (uintptr_t) NULL;
     *(memNode *)(block->next) = newBlock; //lol wat (does this werk?)
-    
+    print_heap(stdout);
     return block; //do we need to return this, or should we figure out a new way
 }
 
@@ -62,7 +66,7 @@ memNode * chooseBlock(memNode * block, memNode * prevBlock, uintptr_t size) {
         else {
             //also needs to account for if getmem is called on an amount
             //larger than LARGE_BLOCK_SIZE
-            return chooseBlock(mallocData(block), NULL,size); //errr might need to link things
+            return chooseBlock(mallocData(block, size), NULL ,size); //errr might need to link things
         }
     }
     else if (root->size > maxOveragePercent*size) {
@@ -81,8 +85,15 @@ void removeFromFree(memNode * block) {
     //remove a memNode wheee
 }
 
-memNode * mallocData(memNode * block) {
-    root = malloc(LARGE_BLOCK_SIZE);
-    root->size = LARGE_BLOCK_SIZE-sizeof(root); //should be 16 less
-    return root;
+memNode * mallocData(memNode * block, uintptr_t size) {
+    if (size > LARGE_BLOCK_SIZE) {
+        block = malloc(size);
+        block->size = LARGE_BLOCK_SIZE-sizeof(memNode);
+    }
+    else {
+        block = malloc(LARGE_BLOCK_SIZE);
+        block->size = LARGE_BLOCK_SIZE-sizeof(memNode); //should be 16 less
+    }
+    printf("%d \n ", LARGE_BLOCK_SIZE);
+    return block;
 }
