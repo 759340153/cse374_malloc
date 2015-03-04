01a06a9238f74adf5a73b15690f5a6a4089186be
Maximilian Golub <mgolub2@uw.edu>
2015-03-03 19:32:49 -0800

    Simple print heap implementation

--- print_heap.c
+++ print_heap.c
@@ -8,10 +8,18 @@
 
 #include <stdio.h>
 #include "mem_impl.h"
+extern memNode * root; //figure out a way to put this in mem_impl.h
 
 void print_heap(FILE * f);
 
 void print_heap(FILE * f)  {
-    //print delicouis heap here
-    //NUM NUM HEAPS
+    if (!f) {
+        fprintf(stderr, "Error writing to file!");
+        exit(1);
+    }
+    memNode * currentNode = root;
+    while(currentNode->next) {
+        fprintf(f, "%lu %lu, ", currentNode, currentNode->size);
+        currentNode = (memNode *) currentNode->next;
+    }
 }
