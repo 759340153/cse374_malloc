00a12237c859d7f072d11b794eaf0594f334177e
Maximilian Golub <mgolub2@uw.edu>
2015-03-03 23:35:05 -0800

    Added more info fields

--- print_heap.c
+++ print_heap.c
@@ -8,6 +8,7 @@
 
 #include <stdio.h>
 #include "mem_impl.h"
+#define ALIGN_VAL 16 //move into mem-impl.h
 extern memNode * root; //figure out a way to put this in mem_impl.h
 
 void print_heap(FILE * f);
@@ -18,8 +19,14 @@ void print_heap(FILE * f)  {
         exit(1);
     }
     memNode * currentNode = root;
-    while(currentNode->next) {
-        fprintf(f, "%lu %lu, ", currentNode, currentNode->size);
+    while(currentNode) {
+        fprintf(f, "0x%08lx 0x%08lx %lu 0x%08lx %lu %lu\n",
+                (long) currentNode,
+                (long) currentNode->size,
+                currentNode->size,
+                (long) currentNode->next,
+                (uintptr_t) currentNode % ALIGN_VAL,
+                currentNode->size % ALIGN_VAL);
         currentNode = (memNode *) currentNode->next;
     }
 }
