1cb77c5198ade6feef66e6dedd02550dabe4b916
Maximilian Golub <mgolub2@uw.edu>
2015-03-03 16:38:50 -0800

    Actually implemented removing nodes from the freelist. TODO - fix what happens when you request a hugely gigantic sum of memory

--- getmem.c
+++ getmem.c
@@ -10,18 +10,19 @@
 #include "mem_impl.h"
 #include "mem.h"
 
+#define ALIGN 16
 #define SPLIT_SIZE 256 //what size we split a block at
 #define LARGE_BLOCK_SIZE 1024 * 16 //size of initial block
-#define ALIGN 16
 #define maxOveragePercent .20
 
 //funtion protos
 memNode * splitBlock(memNode * block, uintptr_t Splitsize); //split a block into many blocks
 void removeFromFree(memNode * block); //remove a memnode from the freelist
 memNode * mallocData(memNode * block); //give memory to a block
-memNode * chooseBlock(memNode * block, uintptr_t size);
+memNode * chooseBlock(memNode * block, memNode * prevBlock, uintptr_t size);
 //globals
 memNode * root;
+
 int totalFree = 0;
 int usedMem = 0;
 int freeMem = 0;
@@ -30,8 +31,9 @@ int freeMem = 0;
 void* getmem(uintptr_t size) {
     if (!root) {
         mallocData(root);
+        //current = root;
     }
-    memNode * choosenBlock = chooseBlock(root, size);
+    memNode * choosenBlock = chooseBlock(root, NULL, size);
     return (void *) &choosenBlock->next+1; //should be start of data
 }
 
@@ -48,26 +50,31 @@ memNode * splitBlock(memNode * block, uintptr_t splitSize) {
     newBlock.size = oldSize - splitSize; // set the new size.
     newBlock.next = (uintptr_t) NULL;
     *(memNode *)(block->next) = newBlock; //lol wat (does this werk?)
+    
     return block; //do we need to return this, or should we figure out a new way
 }
 
-memNode * chooseBlock(memNode * block, uintptr_t size) {
-    if (block->size < size) { //do we need both? hmmm
+memNode * chooseBlock(memNode * block, memNode * prevBlock, uintptr_t size) {
+    if (block->size < size) {
         if (block->size) {
-           return chooseBlock((memNode *) block->next, size);
+           return chooseBlock((memNode *) block->next, block, size);
         }
         else {
-            //also need sto account for if getmem is called on an amount
+            //also needs to account for if getmem is called on an amount
             //larger than LARGE_BLOCK_SIZE
-            return chooseBlock(mallocData(block), size); //errr might need to link things
+            return chooseBlock(mallocData(block), NULL,size); //errr might need to link things
         }
     }
     else if (root->size > maxOveragePercent*size) {
-        return splitBlock(block, size);
+        splitBlock(block, size);
+    }
+    if (prevBlock) {
+        prevBlock->next = block->next; //remove block from linked list
     }
     else {
-        return block;
+        root = (memNode *) block->next;
     }
+    return block;
 }
 
 void removeFromFree(memNode * block) {
