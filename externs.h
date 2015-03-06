9c2a5cdabccc06c5e03dc837688c4650e4349af9
Alex Gingras <agingras@u.washington.edu>
2015-03-06 13:18:55 -0800

    added comments

--- externs.h
+++ externs.h
@@ -1,3 +1,9 @@
+//Alex Gingras and Max Golub
+//CSE 374 HW 6
+//3/4/2015
+//externs.h defines external variables that are used/updated in other source 
+//files.  
+
 extern memNode * root;
 extern int totalSize;
 extern int totalFree;
